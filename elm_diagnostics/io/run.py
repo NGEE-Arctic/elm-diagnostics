99046e13c2638204dc49599b277405fb3fbb835f
Matthew Hoffman <mhoffman@lanl.gov>
2026-05-27 11:25:23 -0600

    Set default join option to 'outer' in Run class dataset loading

--- elm_diagnostics/io/run.py
+++ elm_diagnostics/io/run.py
@@ -169,6 +169,7 @@ class Run:
                 # Avoid requiring dask when not explicitly requested
                 kwargs["chunks"] = None
             kwargs.setdefault("compat", "no_conflicts")
+            kwargs.setdefault("join", "outer")
             self._datasets[tape] = xr.open_mfdataset(files, **kwargs)
             self._cadence[tape] = _infer_cadence(self._datasets[tape])
         return self._datasets[tape]
